30135f08a390ee35e33f0e4cc9ef1022caa384df
x00159358 <x00159358@mytudublin.ie>
2023-09-25 04:14:54 +0100

    updated code

--- src/main/java/com/tudublin/invoice/app/scheduler/PostRequest.java
+++ src/main/java/com/tudublin/invoice/app/scheduler/PostRequest.java
@@ -4,7 +4,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class PostRequest {
@@ -13,19 +16,29 @@ public class PostRequest {
             .version(HttpClient.Version.HTTP_1_1)
             .connectTimeout(Duration.ofSeconds(10))
             .build();
+    private static final List<String> postTypesSupported = Arrays.asList("JSON", "CLOUDEVENT"); 
     private static final String ENDPOINT = Optional.ofNullable(System.getenv("ENDPOINT")).orElse("https://invoicemgr.northeurope-1.eventgrid.azure.net/api/events?overload=cloudEvent&api-version=2018-01-01");
     private static final String CID = Optional.ofNullable(System.getenv("CID")).orElse("uWKbOUKZ/qoFGnHFiEugJCAqzRcr8R+pWRo7szJibTE=");
-    private static final String CONTENT_TYPE = Optional.ofNullable(System.getenv("CONTENT_TYPE")).orElse("application/cloudevents-batch+json;charset=utf-8");
+    private static final String POST_TYPE = Optional.ofNullable("POST_TYPE").filter(x-> postTypesSupported.contains(x)).orElse("JSON");
 
     public static void post(String id){
+        if (POST_TYPE.equalsIgnoreCase("JSON")) {
+            postAsJson(id);
+        } else {
+            postCloudEvent(id);
+        }
+    }
+    
+    
+    public static void postCloudEvent(String id){
         try {
                 final byte[] sampleData = Cloudevent.createCEAsJson().getBytes();
                 final HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
                         .uri(new URI(ENDPOINT))
                         .POST(HttpRequest.BodyPublishers.ofByteArray(sampleData));
-              //  requestBuilder.setHeader("Content-Type", CONTENT_TYPE);
-             //   requestBuilder.setHeader("aeg-sas-key", CID);
-                final HttpRequest request = decorateHeader(requestBuilder);
+                requestBuilder.setHeader("Content-Type", "application/cloudevents-batch+json;charset=utf-8");
+                requestBuilder.setHeader("aeg-sas-key", CID);
+                final HttpRequest request = requestBuilder.build();
                 System.out.println("Posting data for id "+ id);
                 final HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
                 System.out.println(Thread.currentThread().getName() + " ID ="+id+"   RESPONSE "+ response.statusCode()  );
@@ -34,13 +47,27 @@ public class PostRequest {
         }
     }
 
-    public static HttpRequest decorateHeader(final HttpRequest.Builder requestBuilder){
+     public static void postAsJson(String id){
+        try {
+                final HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+                        .uri(new URI(ENDPOINT))
+                        .POST(HttpRequest.BodyPublishers.ofString(Cloudevent.createData(), Charset.defaultCharset()));
+                final HttpRequest request = decorateHeader(requestBuilder, "application/json");
+                System.out.println("Posting data for id "+ id);
+                final HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+                System.out.println(Thread.currentThread().getName() + " ID ="+id+"   RESPONSE "+ response.statusCode()  );
+            }catch (Exception exception){
+                exception.printStackTrace();
+        }
+    }
+
+    public static HttpRequest decorateHeader(final HttpRequest.Builder requestBuilder, String contentType){
 
         requestBuilder.setHeader("Ce-Id", "test");
         requestBuilder.setHeader("Ce-Type", "InvoiceGenRequest");
         requestBuilder.setHeader("Ce-Source", "Invoice.scheduler");
         requestBuilder.setHeader("Ce-Subject", "Invoice.Gen.Req");
-        requestBuilder.setHeader("Content-Type", CONTENT_TYPE);
+        requestBuilder.setHeader("Content-Type", contentType);
         requestBuilder.setHeader("Ce-Specversion", "1.0");
         requestBuilder.setHeader("aeg-sas-key", CID);
         return requestBuilder.build();
